de2df3089876522e488ae6596af662bd26d9eef0
yididiya1 <39265570+yididiya1@users.noreply.github.com>
2021-12-10 16:03:09 +0300

    Create number_of_goodpairs.py

---
+++ week-1/number_of_goodpairs.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def numIdenticalPairs(self, nums):
+        counter = 0
+        for i in range(len(nums)):
+            for j in range(i,len(nums)):
+                if nums[i] == nums[j] and i < j:
+                    counter+=1
+        return counter
