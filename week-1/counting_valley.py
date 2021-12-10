af8960c38a1b4631abe14e0c9c50c3d871e2276b
yididiya1 <39265570+yididiya1@users.noreply.github.com>
2021-12-10 16:16:41 +0300

    Create counting_valley.py

---
+++ week-1/counting_valley.py
@@ -0,0 +1,19 @@
+
+def countingValleys(steps, path):
+    
+    paths = list(path)
+    sealevel = 0;
+    valley = 0
+    
+    for i in range(steps):
+        if paths[i] == 'U':
+            newsealevel = sealevel + 1
+            if newsealevel < 0 and sealevel >= 0:
+                valley+=1
+            sealevel +=1
+        if paths[i] == 'D':
+            newsealevel = sealevel - 1
+            if newsealevel < 0 and sealevel >= 0:
+                valley+=1
+            sealevel -=1
+    return valley
