890178d4fca4c22c127fbd2421d4d957b9ba4ff5
yididiya1 <39265570+yididiya1@users.noreply.github.com>
2022-03-09 21:44:54 +0300

    Time: 35 ms (77.94%), Space: 13.8 MB (73.92%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -2,13 +2,19 @@ class Solution:
     def fib(self, n: int) -> int:
         
         
+        memo = {}
+        
         def fibo(n):
             if n== 0:
                 return 0
             elif n== 1:
                 return 1
             else :
-                return fibo(n-2) + fibo(n-1)
+                prev = memo[n-1] if n-1 in memo else fibo(n-1)
+                prevprev = memo[n-2] if n-2 in memo else fibo(n-2)
+                
+                memo[n] = prev+prevprev
+                return prev + prevprev
         
         
         return fibo(n)
