8755cf60e8f661df3289414e360001b315ffeed0
yididiya1 <39265570+yididiya1@users.noreply.github.com>
2022-03-10 15:35:20 +0300

    Time: 48 ms (99.68%), Space: 14 MB (87.18%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -2,35 +2,35 @@ class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         
         
-#         for i in range(len(cost)-3,-1,-1):
-#             cost[i] += min(cost[i+1],cost[i+2])
+        for i in range(len(cost)-3,-1,-1):
+            cost[i] += min(cost[i+1],cost[i+2])
             
-#         return min(cost[0],cost[1])
+        return min(cost[0],cost[1])
         
     
-        memo = {}
+#         memo = {}
         
-        def dp(index):
-            if index in memo:
-                return memo[index]
+#         def dp(index):
+#             if index in memo:
+#                 return memo[index]
             
-            if index == len(cost)-1:
-                return cost[index]
-            if index == len(cost) -2:
-                return min(cost[-1]+cost[-2],cost[-2])
+#             if index == len(cost)-1:
+#                 return cost[index]
+#             if index == len(cost) -2:
+#                 return min(cost[-1]+cost[-2],cost[-2])
             
             
-#             _next = memo[index+1] if index+1 in memo else dp(index+1)
-#             _nextnext = memo[index+2] if index+2 in memo else dp(index+2)
+# #             _next = memo[index+1] if index+1 in memo else dp(index+1)
+# #             _nextnext = memo[index+2] if index+2 in memo else dp(index+2)
             
-            memo[index] = cost[index] + min(dp(index+1),dp(index+2))
+#             memo[index] = cost[index] + min(dp(index+1),dp(index+2))
             
-            return memo[index]
+#             return memo[index]
             
             
         
-        # print(dp(0),dp(1))
-        return min(dp(0),dp(1))
+#         # print(dp(0),dp(1))
+#         return min(dp(0),dp(1))
         
         
              
