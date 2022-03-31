321e3c60fc48e59ba2a1d9f6711af771e33a2d23
yididiya1 <39265570+yididiya1@users.noreply.github.com>
2022-03-31 19:20:51 +0300

    Time: 80 ms (74.72%), Space: 17.2 MB (5.21%) - LeetHub

--- 120-triangle/120-triangle.py
+++ 120-triangle/120-triangle.py
@@ -4,31 +4,30 @@ class Solution:
     
         
     
-#         memo = {}
+        memo = {}
         
         
-#         def dp(row,index):
-#             # print(row,index)
-#             if row == len(triangle)-1:
-#                 return triangle[row][index]
+        def dp(row,index):
+            if row == len(triangle)-1:
+                return triangle[row][index]
             
             
-#             _next = memo[(row+1,index)] if (row+1,index) in memo else dp(row+1,index)
-#             _next2 = memo[(row+1,index+1)] if (row+1,index+1) in memo else  dp(row+1,index+1)
+            _next = memo[(row+1,index)] if (row+1,index) in memo else dp(row+1,index)
+            _next2 = memo[(row+1,index+1)] if (row+1,index+1) in memo else  dp(row+1,index+1)
             
-#             memo[(row,index)] = triangle[row][index]+min(_next,_next2)
+            memo[(row,index)] = triangle[row][index]+min(_next,_next2)
             
-#             return memo[(row,index)]
+            return memo[(row,index)]
         
         
-#         return (dp(0,0))
+        return (dp(0,0))
 
         
-        for arr in range(len(triangle)-2,-1,-1):
-            for i in range(len(triangle[arr])):
-                triangle[arr][i] +=  min(triangle[arr+1][i+1],triangle[arr+1][i])
+#         for arr in range(len(triangle)-2,-1,-1):
+#             for i in range(len(triangle[arr])):
+#                 triangle[arr][i] +=  min(triangle[arr+1][i+1],triangle[arr+1][i])
                 
-        return (triangle[0][0])
+#         return (triangle[0][0])
                 
                 
             
