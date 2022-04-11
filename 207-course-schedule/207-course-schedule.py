fdea4054c5c60ed50a1299721a2b26082ccdacb0
yididiya1 <39265570+yididiya1@users.noreply.github.com>
2022-04-11 15:06:31 +0300

    Time: 116 ms (72.15%), Space: 16.9 MB (49.47%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,36 +1,75 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        #DFS
+        neighbors = {i:[] for i in range(numCourses)}
         
-        indegree = {}
-        neighbors = {}
-        
-        for i in range(numCourses):
-            indegree[i] = 0
-            neighbors[i] = set()
-            
         for start,dst in prerequisites:
-            indegree[dst] += 1
-            neighbors[start].add(dst)
+            neighbors[start].append(dst)
             
-        queue = deque()
+        visited = set()
+        
+        def dfs(start):
+            if start in visited:
+                return False
+            if neighbors[start] == []:
+                return True
+            
+            visited.add(start)
+            for dst in neighbors[start]:
+                if not dfs(dst):
+                    return False
+            visited.remove(start)
+            neighbors[start] = []
+            
+            return True    
         
         for i in range(numCourses):
-            if indegree[i] == 0:
-                queue.append(i)
+            if dfs(i) == False:
+                return False
+        
+        return True
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+# BFS
+#         indegree = {}
+#         neighbors = {}
+        
+#         for i in range(numCourses):
+#             indegree[i] = 0
+#             neighbors[i] = set()
+            
+#         for start,dst in prerequisites:
+#             indegree[dst] += 1
+#             neighbors[start].add(dst)
+            
+#         queue = deque()
+        
+#         for i in range(numCourses):
+#             if indegree[i] == 0:
+#                 queue.append(i)
                 
-        top_sort = []    
+#         top_sort = []    
         
-        while queue:
-            curr = queue.popleft()
-            top_sort.append(curr)
-            for node in neighbors[curr]:
-                indegree[node] -= 1
-                if indegree[node] == 0:
-                    queue.append(node)
+#         while queue:
+#             curr = queue.popleft()
+#             top_sort.append(curr)
+#             for node in neighbors[curr]:
+#                 indegree[node] -= 1
+#                 if indegree[node] == 0:
+#                     queue.append(node)
         
-        if len(top_sort) == numCourses:
-            return True
-        return False
+#         if len(top_sort) == numCourses:
+#             return True
+#         return False
         
         
         
