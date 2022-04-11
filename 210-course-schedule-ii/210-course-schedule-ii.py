50512a3ce0bdba4386d26ff3770fe7aaa0d50c50
yididiya1 <39265570+yididiya1@users.noreply.github.com>
2022-04-11 16:05:19 +0300

    Time: 134 ms (58.72%), Space: 17.6 MB (16.21%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.py
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -1,32 +1,68 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         
-        neighbors = {i:[] for i in range(numCourses)}
-        queue = deque()
-        indegree = {i:0 for i in range(numCourses)}
+#         neighbors = {i:[] for i in range(numCourses)}
+#         queue = deque()
+#         indegree = {i:0 for i in range(numCourses)}
+        
+#         for start,dst in prerequisites:
+#             neighbors[start].append(dst)
+#             indegree[dst] += 1
+        
+#         for i in range(numCourses):
+#             if indegree[i] == 0:
+#                 queue.append(i)
+        
+        
+#         top_sort  = []
+        
+#         while queue:
+#             start = queue.popleft()    
+#             top_sort.append(start)
+#             for dst in neighbors[start]:
+#                 indegree[dst] -= 1
+#                 if indegree[dst] == 0:
+#                     queue.append(dst)
+        
+#         if len(top_sort) == numCourses:
+#             return top_sort[::-1]
+#         return []    
+
+        neighbors = { i : [] for i in range(numCourses)}
+        visited = set()
+        added = set()
+        
+        
+        top_sort = []
         
         for start,dst in prerequisites:
             neighbors[start].append(dst)
-            indegree[dst] += 1
-        
-        for i in range(numCourses):
-            if indegree[i] == 0:
-                queue.append(i)
-        
-        
-        top_sort  = []
-        
-        while queue:
-            start = queue.popleft()    
-            top_sort.append(start)
+            
+        def dfs(start):
+            if start in visited:
+                return False
+            if start in added:
+                return True
+                
+            visited.add(start)
+            
             for dst in neighbors[start]:
-                indegree[dst] -= 1
-                if indegree[dst] == 0:
-                    queue.append(dst)
-        
-        if len(top_sort) == numCourses:
-            return top_sort[::-1]
-        return []                
+                if not dfs(dst):
+                    return False
+            
+            visited.remove(start)
+            added.add(start)
+            top_sort.append(start)
+            
+            return True
+            
+        for i in range(numCourses):
+            if dfs(i) == False:
+                return []
+            
+        return top_sort
+            
+            
         
         
         
