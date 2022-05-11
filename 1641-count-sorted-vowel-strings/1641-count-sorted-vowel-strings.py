36aa31b45468207aa7715ec0a5b8be3bf7f381e4
yididiya1 <39265570+yididiya1@users.noreply.github.com>
2022-05-11 11:16:45 +0300

    Time: 40 ms (62.13%), Space: 14.1 MB (10.55%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.py
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.py
@@ -1,7 +1,7 @@
 class Solution:
     def countVowelStrings(self, n: int) -> int:
         
-        
+        memo = {}
         
         def dp(n,last=''):
             if n == 0:
@@ -12,8 +12,10 @@ class Solution:
 
                 for i in ['a','e','i','o','u']:
                     if i >= last:
-                        childsum += dp(n-1,i)
-
+                        x = memo[(n-1,i)] if (n-1,i) in memo else dp(n-1,i)
+                        childsum += x
+                
+                memo[(n,last)] = childsum
                 return childsum
         
         return dp(n)
