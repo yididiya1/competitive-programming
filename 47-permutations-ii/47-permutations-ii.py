a3e49354e43ce622665fa4acc54093e996010a74
yididiya1 <39265570+yididiya1@users.noreply.github.com>
2022-07-26 06:52:26 +0300

    Time: 57 ms (96.63%), Space: 14.4 MB (22.16%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.py
+++ 47-permutations-ii/47-permutations-ii.py
@@ -1,20 +1,43 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        perm = []
+#         res = []
+#         perm = []
+#         freq = Counter(nums)
+        
+#         def backtrack():
+#             if len(perm) == len(nums):
+#                 res.append(perm.copy())
+                
+#             for n in freq:
+#                 if freq[n] > 0:
+#                     perm.append(n)
+#                     freq[n] -= 1
+#                     backtrack()
+#                     freq[n] += 1
+#                     perm.pop()
+                
+#         backtrack()
+#         return res
+
+
+        output = []
         freq = Counter(nums)
         
-        def backtrack():
-            if len(perm) == len(nums):
-                res.append(perm.copy())
-                
+        def backtrack(curr):
+            if len(curr) == len(nums):
+                # print(curr)
+                output.append(curr.copy())
+            
+            if len(curr) > len(nums):
+                return
+            
             for n in freq:
                 if freq[n] > 0:
-                    perm.append(n)
+                    curr.append(n)
                     freq[n] -= 1
-                    backtrack()
+                    backtrack(curr)
                     freq[n] += 1
-                    perm.pop()
-                
-        backtrack()
-        return res
+                    curr.pop()
+                    
+        backtrack([])
+        return output
