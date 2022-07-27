3f6d66abd03cf59aa5ff9400d032dccf10300203
yididiya1 <39265570+yididiya1@users.noreply.github.com>
2022-07-27 09:19:49 +0300

    Time: 36 ms (85.07%), Space: 13.9 MB (19.08%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -4,19 +4,31 @@ class Solution:
         memo = {}
         
         #top-down approach 
-        def dp(index):
-            if index == len(nums) - 1:
-                return nums[index]
-            if index == len(nums) - 2:
-                return max(nums[index+1],nums[index])
+#         def dp(index):
+#             if index == len(nums) - 1:
+#                 return nums[index]
+#             if index == len(nums) - 2:
+#                 return max(nums[index+1],nums[index])
             
-            if index in memo : return memo[index]
+#             if index in memo : return memo[index]
             
-            rob = dp(index+2) + nums[index]
-            not_rob = dp(index+1) + 0
+#             rob = dp(index+2) + nums[index]
+#             not_rob = dp(index+1) + 0
             
-            memo[index] = max(rob,not_rob)
+#             memo[index] = max(rob,not_rob)
             
-            return max(rob,not_rob)
+#             return max(rob,not_rob)
         
-        return dp(0)
+#         return dp(0)
+    
+        #bottom-up appraoch
+        
+        if len(nums) == 1: return nums[0]
+        
+        nums[1] = max(nums[1],nums[0])
+        
+        for i in range(2,len(nums)):
+            nums[i] = max((nums[i]+nums[i-2]),(nums[i-1]))
+        
+        # print(nums)
+        return nums[-1]
