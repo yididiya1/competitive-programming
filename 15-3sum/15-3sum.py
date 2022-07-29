d494647d5df16604fce88dea0a3283e6d77aef16
yididiya1 <39265570+yididiya1@users.noreply.github.com>
2022-07-29 10:44:23 +0300

    Time: 1251 ms (55.52%), Space: 18.1 MB (41.74%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,37 +1,54 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        
         nums.sort()
         
-        output = []
-        
-        prev = float('-inf')
-        
-        for i in range(len(nums)-1):
-            if nums[i] == prev:
-                continue
-            target = 0 - nums[i]
-            # print(i,target)
-            l = i+1
-            r = len(nums) - 1
-            prevl = float('-inf')
-            prevr = float('-inf')
+        def two_sum(numss,target):
+            l = 0
+            r = len(numss) - 1
+            
+            answers = []
             
             while l < r:
-                if nums[l] + nums[r] == target:
-                    if prevl != nums[l] and prevr != nums[r]:
-                        output.append([nums[i],nums[l],nums[r]])
-                        prevl = nums[l]
-                        prevr = nums[r]
-                    l += 1
+                if numss[l] + numss[r] == target :
+                    answers.append([numss[l],numss[r]])
+                    while r-1 > l and numss[r-1] == numss[r]:
+                        r -= 1
+                    while l + 1 < r and numss[l+1] == numss[l]:
+                        l += 1  
                     r -= 1
-                elif nums[l] + nums[r] > target:
+                    l += 1
+                    
+                elif numss[l] + numss[r] > target :
                     r -= 1
                 else:
                     l += 1
-            
-            # print(res,"=====")
-            prev = nums[i]
-                
+                    
+            return answers
         
-        return output
+        
+        output = []
+        
+        i = 0
+        
+        
+        while i < len(nums):
+            target = 0 - nums[i]
+            check = two_sum(nums[i+1:],target)
+            # print(check,i)
+            if check :
+                for z in check:
+                    output.append([nums[i],z[0],z[1]])
+                
+                while i + 1< len(nums) and  nums[i] == nums[i+1]:
+                    i += 1
+            
+            i += 1
+            
+        return output    
+            
+        
+        
+        
+        
         
