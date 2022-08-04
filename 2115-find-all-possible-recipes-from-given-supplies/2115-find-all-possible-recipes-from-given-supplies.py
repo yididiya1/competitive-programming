7869e96fd36e681acc81e3424242d9772a97c732
yididiya1 <39265570+yididiya1@users.noreply.github.com>
2022-08-04 22:28:21 +0300

    Time: 1033 ms (85.43%), Space: 17.1 MB (69.19%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
@@ -1,37 +1,36 @@
 class Solution:
     def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
         requires = {recipe:[] for recipe in recipes}
-        indegree =  [0 for i in range(len(recipes))]
+        indegree = [0] * len(recipes)
         supplies = set(supplies)
         
         for i in range(len(ingredients)):
             for j in range(len(ingredients[i])):
-                if  ingredients[i][j] not in supplies  :
+                if ingredients[i][j] not in supplies:
                     if ingredients[i][j] in requires:
                         requires[ingredients[i][j]].append(i)
                     indegree[i] += 1
-                    # requires[ingredients[i][j]].append(i)     
-                
+            
+        
+        queue = deque()
         
         
-        
-        queue = deque()            
-        output = []
         
         for i in range(len(indegree)):
             if indegree[i] == 0:
                 queue.append(i)
-        
+         
+        # print(queue)
+        answer = []
         while queue:
-            recipe = queue.popleft()
-            output.append(recipes[recipe])
+            rec = queue.popleft()
+            answer.append(recipes[rec])
             
-            for i in requires[recipes[recipe]]:
+            for i in requires[recipes[rec]]:
                 indegree[i] -= 1
                 if indegree[i] == 0:
                     queue.append(i)
                     
-        return output
-                
+        return answer
             
-             
+        
