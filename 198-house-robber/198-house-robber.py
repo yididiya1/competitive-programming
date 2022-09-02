ecdba7fcac2b3d76f2c54b25c15e8af37f750c45
yididiya1 <39265570+yididiya1@users.noreply.github.com>
2022-09-02 11:09:14 +0300

    Time: 49 ms (52.12%), Space: 13.8 MB (97.19%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,34 +1,14 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
-        memo = {}
-        
-        #top-down approach 
-#         def dp(index):
-#             if index == len(nums) - 1:
-#                 return nums[index]
-#             if index == len(nums) - 2:
-#                 return max(nums[index+1],nums[index])
+        @cache
+        def dp(index):
+            if index >= len(nums):
+                return 0
             
-#             if index in memo : return memo[index]
+            rob = nums[index]+dp(index+2)
+            not_rob = dp(index+1)
             
-#             rob = dp(index+2) + nums[index]
-#             not_rob = dp(index+1) + 0
-            
-#             memo[index] = max(rob,not_rob)
-            
-#             return max(rob,not_rob)
+            return max(rob,not_rob)
         
-#         return dp(0)
-    
-        #bottom-up appraoch
-        
-        if len(nums) == 1: return nums[0]
-        
-        nums[1] = max(nums[1],nums[0])
-        
-        for i in range(2,len(nums)):
-            nums[i] = max((nums[i]+nums[i-2]),(nums[i-1]))
-        
-        # print(nums)
-        return nums[-1]
+        return dp(0)
