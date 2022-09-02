2880ff12b04fc383769e2682b00bd100847a94b7
yididiya1 <39265570+yididiya1@users.noreply.github.com>
2022-09-02 11:34:17 +0300

    Time: 73 ms (5.36%), Space: 14.2 MB (5.15%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.py
+++ 213-house-robber-ii/213-house-robber-ii.py
@@ -1,29 +1,25 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
-        
         if len(nums) == 1:
             return nums[0]
         
-        def getMax(numss):
-            
-            memo = {}
-            
+        curr = nums[:-1].copy()
+        
+        def getMax(arr):
+
+            @cache
             def dp(index):
-                if index == len(numss) - 1:
-                    return numss[index]
-                if index == len(numss) - 2:
-                    return max(numss[index+1],numss[index])
-
-                if index in memo : return memo[index]
-
-                rob = dp(index+2) + numss[index]
-                not_rob = dp(index+1) + 0
-
-                memo[index] = max(rob,not_rob)
+                if index >= len(arr):
+                    return 0
+                rob = arr[index]+dp(index+2)
+                not_rob = dp(index+1)
 
                 return max(rob,not_rob)
-
+            
             return dp(0)
+       
+        return max(getMax(nums[:-1]),getMax(nums[1:]))
         
-        return max(getMax(nums[1:]),getMax(nums[:-1]))
+        
+        
