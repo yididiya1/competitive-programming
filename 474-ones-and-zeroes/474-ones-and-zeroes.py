31b9c61c7fbcc73f34f1661c51d5da9716a03f0d
yididiya1 <39265570+yididiya1@users.noreply.github.com>
2022-09-22 10:36:45 +0300

    Time: 4046 ms (74.32%), Space: 192.7 MB (23.11%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.py
+++ 474-ones-and-zeroes/474-ones-and-zeroes.py
@@ -2,27 +2,17 @@ class Solution:
     def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
         
         
-        count = []
-        for st in strs:
-            x = st.count('0')
-            y = st.count('1')
-            count.append([x,y])
-        
-        # print(count)
-        
+        count = [[st.count('0'),st.count('1')] for st in strs]
+
+       
         @cache
         def dp(index,one,zero):
             if index >= len(strs):
                 return 0
             
-            # if one-count[index][1] < 0 or zero - count[index][0] < 0:
-            #     print("Here",index,one,zero)
-            #     return 0
-            
             take_me = 0 if one-count[index][1] < 0 or zero - count[index][0] < 0 else 1 + dp(index+1,one-count[index][1],zero-count[index][0])
             skip_me = dp(index+1,one,zero)
             
-            # print(index,max(take_me,skip_me),zero,one)
             return max(take_me,skip_me)
         
         
