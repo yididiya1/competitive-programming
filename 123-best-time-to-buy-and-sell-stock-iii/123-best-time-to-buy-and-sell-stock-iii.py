d62b481de3e1cd097bab14bae0b95981524f66f1
yididiya1 <39265570+yididiya1@users.noreply.github.com>
2022-09-27 15:34:57 +0300

    Time: 4502 ms (18.84%), Space: 28.9 MB (51.56%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.py
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.py
@@ -1,23 +1,21 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         
-        @cache
-        def dp(index,canBuy,left):
-            if index >= len(prices):
-                return 0
-            
-            
-            buy,not_buy = 0,0
-            sell,not_sell = 0,0
-            
-            if canBuy and left > 0:
-                buy = -prices[index] +  dp(index+1,False,left)
-                not_buy = dp(index+1,True,left)
-            
-            elif not canBuy:
-                sell = prices[index] + dp(index+1,True,left-1)
-                not_sell = dp(index+1,False,left)
-            
-            return max(buy,not_buy,sell,not_sell)
+        left_min,left_profit = prices[0],[0]
+        right_max = prices[-1]
+        right_profit = [0] * (len(prices) +1)
         
-        return dp(0,True,2)
+        for i in range(1,len(prices)):
+            left_profit.append(max(left_profit[i-1],prices[i]-left_min))
+            left_min = min(left_min,prices[i])
+            
+        for i in range(len(prices)-2,-1,-1):
+            right_profit[i] = max(right_profit[i+1],right_max - prices[i])
+            right_max = max(right_max,prices[i])
+            
+        
+        max_profit = 0
+        for i in range(len(prices)):
+            max_profit = max(max_profit,left_profit[i]+right_profit[i+1])
+            
+        return max_profit
